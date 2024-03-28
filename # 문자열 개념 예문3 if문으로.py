8043c4961a6e065b95216e8c8a422a587334df27
PetricOska <164276701+PetricOska@users.noreply.github.com>
2024-03-28 21:24:54 +0900

    Add files via upload

---
+++ # 문자열 개념 예문3 if문으로.py
@@ -0,0 +1,30 @@
+## 함수 선언 부분 ##
+def calc(숫자1, op, 숫자2) :
+    result = 0
+    if op =='+' :
+        result = 숫자1 + 숫자2
+    elif op =='-' :
+        result = 숫자1 - 숫자2
+    elif op =='*' :
+        result = 숫자1 * 숫자2
+    elif op == '**' :
+        result = 숫자1 ** 숫자2
+    elif op == '/' :
+        if 숫자2 == 0 :
+            return "0으로 나눌 수 없습니다."
+        else:
+            return 숫자1 / 숫자2
+    
+    return result
+
+## 변수 선언 부분 ##
+res=0
+var1, var2, oper = 0, 0, ""
+## 메인 코드 부분 ##
+oper = input("계산을 입력하세요(+, -, *, /, **):")
+var1 = int(input("첫 번째 수를 입력하세요 : "))       
+var2 = int(input("두 번째 수를 입력하세요 : "))
+
+res = calc(var1, var2, oper)
+
+print("## 계산기:%d %s %d = %d" % (var1, oper, var2, res))
